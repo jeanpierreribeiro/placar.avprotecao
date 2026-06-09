6ddaa65d1540d8667e64c5034d600420ac4926bc
Jean Pierre Ribeiro <jeanpierreribeiro.br@gmail.com>
2026-06-09 17:04:25 -0300

    Add Meta Pixel tracking

--- app.vue
+++ app.vue
@@ -340,6 +340,25 @@ useHead({
     { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
     { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
     { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,400;0,600;0,700;0,800;0,900;1,700&family=Barlow:wght@400;500;600&display=swap' }
+  ],
+  script: [
+    {
+      innerHTML: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '672773542259251');
+fbq('track', 'PageView');`
+    }
+  ],
+  noscript: [
+    {
+      innerHTML: '<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=672773542259251&ev=PageView&noscript=1" />'
+    }
   ]
 })
 
